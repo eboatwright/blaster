68a0264faee8f377931e0e7475a4d46c25634d2b
eboatwright <ekb@ranchoburritofarm.com>
2021-06-18 11:56:15 -0500

    6/18/21 11:56 a.m.
    - Finished player movement (is a little sketchy)

--- platformer/src/gameObjects/Player.cs
+++ platformer/src/gameObjects/Player.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -6,19 +7,24 @@ using Microsoft.Xna.Framework.Input;
 namespace eboatwright {
     public class Player : GameObject {
 
-        public const int SPRITE_WIDTH = 12, SPRITE_HEIGHT = 13;
-        public const float MOVE_SPEED = 1f, FRICTION = 0.7f, GRAVITY = 0.4f, JUMP_HEIGHT = -7.2f;
+        public const int COLLISION_WIDTH = 13, COLLISION_HEIGHT = 14;
+        public const float MOVE_SPEED = 1f, FRICTION = 0.7f, GRAVITY = 0.4f, JUMP_HEIGHT = -7.2f, COYOTE_TIME = 8;
+
+        private bool jumpReleased;
+        private float lastGrounded = 0f;
 
         private Vector2 velocity;
 
         private Texture2D playerImg;
 
         private Camera camera;
+        private Map map;
 
         public Player(Scene scene) : base(scene) {}
 
         public override void Initialize() {
             tags.Add("Player");
+            map = (Map)scene.FindGameObjectWithTag("Map");
         }
 
         public override void LoadContent(ContentManager content) {
@@ -28,9 +34,61 @@ namespace eboatwright {
         public override void Update(float deltaTime, MouseState mouse, KeyboardState keyboard) {
             if (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.Left)) velocity.X -= MOVE_SPEED * deltaTime;
             if (keyboard.IsKeyDown(Keys.D) || keyboard.IsKeyDown(Keys.Right)) velocity.X += MOVE_SPEED * deltaTime;
+            if(keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.Up)) {
+                if(jumpReleased) {
+                    jumpReleased = false;
+                    if(lastGrounded > 0f) {
+                        lastGrounded = 0f;
+                        velocity.Y += JUMP_HEIGHT;
+                    }
+                }
+            } else {
+                jumpReleased = true;
+            }
+
+            velocity.Y += GRAVITY * deltaTime;
+            position.Y += velocity.Y * deltaTime;
+            lastGrounded -= deltaTime;
+
+            Rectangle PlayerRectangle = new Rectangle((int)Math.Floor(position.X), (int)Math.Floor(position.Y), COLLISION_WIDTH - 1, COLLISION_HEIGHT);
+
+            for (int y = 0; y <= map.mapValues.GetUpperBound(0); y++)
+                for (int x = 0; x <= map.mapValues.GetUpperBound(1); x++) {
+                    if (map.mapValues[y, x] > 0 && map.mapValues[y, x] < 14) {
+                        Rectangle TileRectangle = new Rectangle(x * Map.TILE_SIZE, y * Map.TILE_SIZE, Map.TILE_SIZE, Map.TILE_SIZE);
+                        if (RectanglesOverlap(PlayerRectangle, TileRectangle)) {
+                            if (velocity.Y > 0f) {
+                                velocity.Y = 0f;
+                                lastGrounded = COYOTE_TIME;
+                                position.Y = y * Map.TILE_SIZE - COLLISION_HEIGHT + 1;
+                            } else if (velocity.Y < 0f) {
+                                velocity.Y = 0f;
+                                position.Y = y * Map.TILE_SIZE + Map.TILE_SIZE;
+                            }
+                        }
+                    }
+                }
 
             velocity.X *= FRICTION;
             position.X += velocity.X;
+
+            PlayerRectangle = new Rectangle((int)Math.Floor(position.X), (int)Math.Floor(position.Y), COLLISION_WIDTH, COLLISION_HEIGHT - 1);
+
+            for (int y = 0; y <= map.mapValues.GetUpperBound(0); y++)
+                for (int x = 0; x <= map.mapValues.GetUpperBound(1); x++) {
+                    if (map.mapValues[y, x] > 0 && map.mapValues[y, x] < 14) {
+                        Rectangle TileRectangle = new Rectangle(x * Map.TILE_SIZE, y * Map.TILE_SIZE, Map.TILE_SIZE, Map.TILE_SIZE);
+                        if (RectanglesOverlap(PlayerRectangle, TileRectangle)) {
+                            if(velocity.X > 0f) {
+                                velocity.X = 0f;
+                                position.X = x * Map.TILE_SIZE - COLLISION_WIDTH + 1;
+                            } else if(velocity.X < 0f) {
+                                velocity.X = 0f;
+                                position.X = x * Map.TILE_SIZE + Map.TILE_SIZE;
+                            }
+                        }
+                    }
+                }
         }
 
         public override void Draw(SpriteBatch spriteBatch) {
@@ -38,11 +96,7 @@ namespace eboatwright {
                 camera = (Camera)scene.FindGameObjectWithTag("Camera");
                 return;
             }
-            spriteBatch.Draw(playerImg, position - camera.scroll, Color.White);
-        }
-
-        public Rectangle GetHitbox() {
-            return new Rectangle((int)position.X, (int)position.Y, SPRITE_WIDTH, SPRITE_HEIGHT);
+            spriteBatch.Draw(playerImg, position - camera.scroll, new Rectangle(0, 0, 12, 13), Color.White);
         }
 
         public bool RectanglesOverlap(Rectangle a, Rectangle b) {
