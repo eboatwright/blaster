d53b9300066db77cee8e1c6b0d9485a93a61ec20
eboatwright <ekb@ranchoburritofarm.com>
2021-06-18 13:02:07 -0500

    6/18/21 1:01 p.m.
    - Added Rect.cs class
    - Refactored Player.cs class to use Rect.cs class

---
+++ platformer/src/engine/Rect.cs
@@ -0,0 +1,26 @@
+﻿namespace eboatwright {
+    public class Rect {
+
+        public int x, y, width, height;
+        public int top, bottom, left, right;
+
+        public Rect(int x, int y, int width, int height) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+
+            this.top = y;
+            this.bottom = y + height;
+            this.left = x;
+            this.right = x + width;
+        }
+
+        public bool Overlaps(Rect other) {
+            return this.left < other.right &&
+                    this.right > other.left &&
+                    this.top < other.bottom &&
+                    this.bottom > other.top;
+        }
+    }
+}
--- platformer/src/gameObjects/Player.cs
+++ platformer/src/gameObjects/Player.cs
@@ -8,7 +8,7 @@ namespace eboatwright {
     public class Player : GameObject {
 
         public const int COLLISION_WIDTH = 13, COLLISION_HEIGHT = 14;
-        public const float MOVE_SPEED = 1f, FRICTION = 0.7f, GRAVITY = 0.4f, JUMP_HEIGHT = -7.2f, COYOTE_TIME = 8;
+        public const float MOVE_SPEED = 0.85f, FRICTION = 0.7f, GRAVITY = 0.34f, JUMP_HEIGHT = -6f, COYOTE_TIME = 8;
 
         private bool jumpReleased;
         private float lastGrounded = 0f;
@@ -20,7 +20,7 @@ namespace eboatwright {
         private Camera camera;
         private Map map;
 
-        public Player(Scene scene) : base(scene) {}
+        public Player(Scene scene) : base(scene) { }
 
         public override void Initialize() {
             tags.Add("Player");
@@ -34,10 +34,10 @@ namespace eboatwright {
         public override void Update(float deltaTime, MouseState mouse, KeyboardState keyboard) {
             if (keyboard.IsKeyDown(Keys.A) || keyboard.IsKeyDown(Keys.Left)) velocity.X -= MOVE_SPEED * deltaTime;
             if (keyboard.IsKeyDown(Keys.D) || keyboard.IsKeyDown(Keys.Right)) velocity.X += MOVE_SPEED * deltaTime;
-            if(keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.Up)) {
-                if(jumpReleased) {
+            if (keyboard.IsKeyDown(Keys.W) || keyboard.IsKeyDown(Keys.Up)) {
+                if (jumpReleased) {
                     jumpReleased = false;
-                    if(lastGrounded > 0f) {
+                    if (lastGrounded > 0f) {
                         lastGrounded = 0f;
                         velocity.Y += JUMP_HEIGHT;
                     }
@@ -50,13 +50,13 @@ namespace eboatwright {
             position.Y += velocity.Y * deltaTime;
             lastGrounded -= deltaTime;
 
-            Rectangle PlayerRectangle = new Rectangle((int)Math.Floor(position.X), (int)Math.Floor(position.Y), COLLISION_WIDTH - 1, COLLISION_HEIGHT);
+            Rect PlayerRect = new Rect((int)Math.Floor(position.X), (int)Math.Floor(position.Y), COLLISION_WIDTH - 1, COLLISION_HEIGHT);
 
             for (int y = 0; y <= map.mapValues.GetUpperBound(0); y++)
                 for (int x = 0; x <= map.mapValues.GetUpperBound(1); x++) {
                     if (map.mapValues[y, x] > 0 && map.mapValues[y, x] < 14) {
-                        Rectangle TileRectangle = new Rectangle(x * Map.TILE_SIZE, y * Map.TILE_SIZE, Map.TILE_SIZE, Map.TILE_SIZE);
-                        if (RectanglesOverlap(PlayerRectangle, TileRectangle)) {
+                        Rect TileRect = new Rect(x * Map.TILE_SIZE, y * Map.TILE_SIZE, Map.TILE_SIZE, Map.TILE_SIZE);
+                        if (PlayerRect.Overlaps(TileRect)) {
                             if (velocity.Y > 0f) {
                                 velocity.Y = 0f;
                                 lastGrounded = COYOTE_TIME;
@@ -72,17 +72,17 @@ namespace eboatwright {
             velocity.X *= FRICTION;
             position.X += velocity.X;
 
-            PlayerRectangle = new Rectangle((int)Math.Floor(position.X), (int)Math.Floor(position.Y), COLLISION_WIDTH, COLLISION_HEIGHT - 1);
+            PlayerRect = new Rect((int)Math.Floor(position.X), (int)Math.Floor(position.Y), COLLISION_WIDTH, COLLISION_HEIGHT - 1);
 
             for (int y = 0; y <= map.mapValues.GetUpperBound(0); y++)
                 for (int x = 0; x <= map.mapValues.GetUpperBound(1); x++) {
                     if (map.mapValues[y, x] > 0 && map.mapValues[y, x] < 14) {
-                        Rectangle TileRectangle = new Rectangle(x * Map.TILE_SIZE, y * Map.TILE_SIZE, Map.TILE_SIZE, Map.TILE_SIZE);
-                        if (RectanglesOverlap(PlayerRectangle, TileRectangle)) {
-                            if(velocity.X > 0f) {
+                        Rect TileRect = new Rect(x * Map.TILE_SIZE, y * Map.TILE_SIZE, Map.TILE_SIZE, Map.TILE_SIZE);
+                        if (PlayerRect.Overlaps(TileRect)) {
+                            if (velocity.X > 0f) {
                                 velocity.X = 0f;
                                 position.X = x * Map.TILE_SIZE - COLLISION_WIDTH + 1;
-                            } else if(velocity.X < 0f) {
+                            } else if (velocity.X < 0f) {
                                 velocity.X = 0f;
                                 position.X = x * Map.TILE_SIZE + Map.TILE_SIZE;
                             }
@@ -97,13 +97,6 @@ namespace eboatwright {
                 return;
             }
             spriteBatch.Draw(playerImg, position - camera.scroll, new Rectangle(0, 0, 12, 13), Color.White);
-        }
-
-        public bool RectanglesOverlap(Rectangle a, Rectangle b) {
-            return a.Left < b.Right &&
-                    a.Right > b.Left &&
-                    a.Top < b.Bottom &&
-                    a.Bottom > b.Top;
         }
     }
 }
